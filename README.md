cce79820236f0a54d92b3d5c13c2f6a4eee5c625
Parag R. Birje <paragrbirje@gmail.com>
2012-08-04 00:12:58 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Demo Application
+
+This is the second application for [*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org) by [*Parag R. Birje*](http://paragbirje.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a526d931ec97d454a9ecef1d387b11293d07f36504320361e4ab591606d2fd5149a08f4338af717f8d658ed71f2677a1046b9901efe986b2064d117fbbbd324e'
